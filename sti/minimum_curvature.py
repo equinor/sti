de4bee3487936420559496c276c928f3fda5c1ef
Jon Gustav Vabø <jgvabo@gmail.com>
2020-08-04 19:36:32 +0200

    Add minimum curvature calculation.

---
+++ sti/minimum_curvature.py
@@ -0,0 +1,58 @@
+from pydantic import BaseModel
+import numpy as np
+
+class SurveyPoint(BaseModel):
+    inc: float
+    azi: float
+    md_inc: float
+
+class MinCurveSegment(BaseModel):
+    dnorth: float
+    deast: float
+    dtvd: float
+    dls: float
+
+def getMinCurveSegment(upperSurvey: SurveyPoint, lowerSurvey: SurveyPoint):
+    """Calculate increments in TVD, northing, easting and associated dogleg
+    using the minimum curvature method.
+
+    Assumes inc and azi in radians. Returns dog leg severity in rad/length.
+
+    Parameters
+    ----------
+    upper: Upper survey point
+    lower: Lower survey point
+
+    Returns
+    -------
+    minCurveSegment: MinCurveSegment
+    """
+    # Stolen from wellpathpy
+    cos_inc = np.cos(lowerSurvey.inc - upperSurvey.inc)
+    sin_inc = np.sin(upperSurvey.inc) * np.sin(lowerSurvey.inc)
+    cos_azi = 1 - np.cos(lowerSurvey.azi - upperSurvey.azi)
+
+    dogleg = np.arccos(cos_inc - (sin_inc * cos_azi))
+    md_inc = lowerSurvey.md_inc
+
+    # ratio factor, correct for dogleg == 0 values
+    with np.errstate(divide = 'ignore', invalid = 'ignore'):
+        rf = 2 / dogleg * np.tan(dogleg / 2)
+        rf = np.where(dogleg == 0., 1, rf)
+    
+    # delta northing
+    upper = np.sin(upperSurvey.inc) * np.cos(upperSurvey.azi)
+    lower = np.sin(lowerSurvey.inc) * np.cos(lowerSurvey.azi)
+    dnorth = md_inc / 2 * (upper + lower) * rf
+    
+    # delta easting
+    upper = np.sin(upperSurvey.inc) * np.sin(upperSurvey.azi)
+    lower = np.sin(lowerSurvey.inc) * np.sin(lowerSurvey.azi)
+    deast = md_inc / 2 * (upper + lower) * rf
+
+    # delta tvd
+    dtvd = md_inc / 2 * (np.cos(upperSurvey.inc) + np.cos(lowerSurvey.inc)) *rf
+
+    minCurveSegment = MinCurveSegment(dnorth=dnorth, deast=deast, dtvd = dtvd, dls=dogleg)
+
+    return minCurveSegment
---
+++ tests/test_minimum_curvature.py
@@ -0,0 +1,117 @@
+from pytest import approx
+import sti.minimum_curvature as mc
+import numpy as np
+
+class TestMinCurveSegment:
+    def test_zero_md_inc(self):
+        upper_data = {
+            'inc' : 0,
+            'azi' : 0,
+            'md_inc'  : 0,
+        }
+        upper = mc.SurveyPoint(**upper_data)
+        lower = upper
+
+        segment = mc.getMinCurveSegment(upper, lower)
+
+        assert segment.dnorth == 0
+        assert segment.deast == 0
+        assert segment.dtvd == 0
+        
+    def test_straight_down(self):
+        upper_data = {
+            'inc' : 0,
+            'azi' : 0,
+            'md_inc'  : 0,
+        }
+        upper = mc.SurveyPoint(**upper_data)
+
+        lower_data = {
+            'inc' : 0,
+            'azi' : 0,
+            'md_inc'  : 123,
+        }
+        lower = mc.SurveyPoint(**lower_data)
+
+        segment = mc.getMinCurveSegment(upper, lower)
+
+        assert segment.dnorth == 0
+        assert segment.deast == 0
+        assert segment.dtvd == 123
+
+
+    def test_straight_east(self):
+        azi = np.pi / 2
+        inc = np.pi / 2
+        upper_data = {
+            'inc' : inc,
+            'azi' : azi,
+            'md_inc'  : 0,
+        }
+        upper = mc.SurveyPoint(**upper_data)
+
+        lower_data = {
+            'inc' : inc,
+            'azi' :  azi,
+            'md_inc'  : 234,
+        }
+        lower = mc.SurveyPoint(**lower_data)
+
+        segment = mc.getMinCurveSegment(upper, lower)
+
+        assert segment.dnorth == approx(0)
+        assert segment.deast == approx(234)
+        assert segment.dtvd == approx(0)
+
+    def test_straight_north(self):
+        azi = 0
+        inc = np.pi / 2
+        upper_data = {
+            'inc' : inc,
+            'azi' : azi,
+            'md_inc'  : 0,
+        }
+        upper = mc.SurveyPoint(**upper_data)
+
+        lower_data = {
+            'inc' : inc,
+            'azi' :  azi,
+            'md_inc'  : 578,
+        }
+        lower = mc.SurveyPoint(**lower_data)
+
+        segment = mc.getMinCurveSegment(upper, lower)
+
+        assert segment.dnorth == approx(578)
+        assert segment.deast == approx(0)
+        assert segment.dtvd == approx(0)
+
+    def circle_downward_east(self):
+        azi = np.pi / 2
+        inc = 0
+        upper_data = {
+            'inc' : inc,
+            'azi' : azi,
+            'md_inc'  : 0,
+        }
+        upper = mc.SurveyPoint(**upper_data)
+
+
+        azi = np.pi / 2
+        inc = np.pi / 2
+        md_inc = 1000
+        lower_data = {
+            'inc' : inc,
+            'azi' :  azi,
+            'md_inc'  : md_inc,
+        }
+        lower = mc.SurveyPoint(**lower_data)
+
+        segment = mc.getMinCurveSegment(upper, lower)
+
+        radius = 2*md_inc/np.pi
+
+        assert segment.dnorth == approx(0)
+        assert segment.deast == approx(radius)
+        assert segment.dtvd == approx(radius)
+
