a05522eb01e36b345782c5c35c5e028d2ded5bec
Jon Gustav Vabø <jgvabo@gmail.com>
2020-08-08 15:19:58 +0200

    Add script to clean bad existing data. Add new clean data.

---
+++ scripts/clean_data.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import sti.sti_core as sti_core
+
+# Script to remove data with bad fit from training
+MAX_ERR = 0.1
+
+# Input data
+filename_data ='data.csv'
+filename_clean = 'data_cleaned.csv'
+
+# Read dataset
+df = pd.read_csv(filename_data)
+
+# Find rows with bad data
+bad = []
+for i in range(0, df.shape[0]):
+    states = np.array(df.iloc[i,0:11].values)
+    from_state = states[0:5]
+    to_state = states[5:10]
+    dls_limit = states[10]
+
+    sti = np.array(df.iloc[i,11:])
+    projection, dls = sti_core.project_sti(from_state, sti)
+    err = sti_core.__err_squared_state_mismatch(to_state, projection, dls_limit, scale_md=100)
+
+    if err > MAX_ERR:
+        print("i: ", i, "err: ", err)
+        bad.append(i)
+
+df = df.drop(bad)
+df.to_csv(filename_clean)
