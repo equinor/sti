cfe38563f034d793d301b88a271dd9b3a4cac5db
Jon Gustav Vabø <jgvabo@gmail.com>
2020-08-11 07:54:41 +0200

    Refactor dogleg toolface method.

--- sti/sti_core.py
+++ sti/sti_core.py
@@ -11,6 +11,7 @@ from sklearn.pipeline import Pipeline
 
 # For QC toolface method
 import matplotlib.pyplot as plt
+from random import random
 
 """
 Given a start and end state in a space of (north, east, tvd, inc, azi),
@@ -391,58 +392,155 @@ def dogleg_toolface(inc_upper, azi_upper, toolface, dls, md_inc):
 
     if toolface < 0 or dls == 0.0:
         # Go straight ahead
-        return min_curve_segment(inc_upper, azi_upper, inc_upper, azi_upper, md_inc)
+        dnorth, deast, dtvd, dls = min_curve_segment(inc_upper, azi_upper, inc_upper, azi_upper, md_inc)
+        return np.array((dnorth, deast, dtvd, inc_upper, azi_upper, toolface))
     else:
-        cos_tf = np.cos(toolface)
-        sin_tf = np.sin(toolface)
-
-        # TODO When theta is approaching pi or larger, the calculations
-        # should be done in pieces to avoid disaster as min_curve breaks down.
-        theta = dls * md_inc
-
-        partial_1 = np.cos(inc_upper) * np.cos(theta)
-        partial_2 = cos_tf * np.sin(inc_upper)*np.sin(theta)
-
-        inc_lower = np.arccos(partial_1 - partial_2)
-        
-        partical_3 = sin_tf * np.sin(theta)  
-
-        delta_azi = 0.0
-        if np.sin(inc_lower) != 0:
-            tan_delta = sin_tf * np.tan(theta) / (np.sin(inc_upper) + np.cos(inc_upper) * cos_tf * np.tan(theta))
-            delta_azi = np.arctan(tan_delta)
-        
-        azi_lower = azi_upper + delta_azi
-
-        if azi_lower < 0:
-            azi_lower = azi_lower + 2*np.pi
+        # TODO Split in multiple calls to handle dls * md_inc > pi
+        return __dogleg_toolface_inner(inc_upper, azi_upper, toolface, dls, md_inc)
 
 
-        # Handle inc_upper 0 or pi
-        # In these cases, the magnetic toolface is used intially
-        # In practice, the result are curves in a specific azimuthal direction,
-        # that is only build, no turn.
-        if inc_upper == 0 or inc_upper == np.pi:
-            azi_lower = toolface
-        
-        dnorth, deast, dtvd, dls = min_curve_segment(inc_upper, azi_upper, inc_lower, azi_lower, md_inc)
-        return np.array((dnorth, deast, dtvd, inc_lower, azi_lower))
-        
-def get_toolface_as_vector(bit_inc, bit_azi, toolface):
+def __dogleg_toolface_inner(inc_upper, azi_upper, toolface, dls, md_inc):
+    """
+    Inner method used by dogleg_toolface to handle arc angles larger
+    than pi.
 
-    raise NotImplementedError
+    Assumes dls>0 and dls * md_inc < pi.
+    """
+    # assert(dls > 0)
+    # assert(dls * md_inc < np.pi)
 
-# if __name__ == '__main__':
+    theta = dls * md_inc
+    sin_tf = np.sin(toolface)
+    cos_tf = np.cos(toolface)
+
+    # Using formulas (6) & (7) from
+    # https://docs.oliasoft.com/technical-documentation/well-trajectory-design
+    partial_1 = np.cos(inc_upper) * np.cos(theta)
+    partial_2 = cos_tf * np.sin(inc_upper)*np.sin(theta)
+
+    inc_lower = np.arccos(partial_1 - partial_2)
     
-    # segment = dogleg_toolface(np.pi, 0., 2*np.pi*3/8, 0.001, 800)
-    # print("South East: ", segment)
+    partical_3 = sin_tf * np.sin(theta)  
 
-    # tf = np.linspace(0,2*np.pi, 100)
-    # inc = np.copy(tf)
-    # azi = np.copy(tf)
+    delta_azi = 0.0
+    # Handle discountinuites by computing both sin & cos deltas, as references above
+    # and using arctan2 to compute the change in azimuth
+    if np.sin(inc_lower) != 0 and np.sin(inc_upper) != 0:
+        tan_delta = sin_tf * np.tan(theta) / (np.sin(inc_upper) + np.cos(inc_upper) * cos_tf * np.tan(theta))
+        sin_theta = np.sin(theta)
+        sin_delta = sin_tf * sin_theta / np.sin(inc_upper)
 
-    # for i in range(0, len(tf)):
-    #     _, _, _, inc[i], azi[i] = dogleg_toolface(np.pi, 0, tf[i],0.001, 250)
+        # Handle tan_delta = 0
+        if tan_delta != 0:
+            cos_delta = sin_delta / tan_delta
+        else:
+            cos_delta = np.cos(azi_upper)
+
+        delta_azi = np.arctan2(sin_delta, cos_delta)
+    else:
+        # By definition, there cannot be any change of azimuth if it 
+        # start or ends with sin(inc) = 0 
+        # TODO Think hard about this and write a better explanation
+        delta_azi = 0.0
+    
+    azi_lower = azi_upper + delta_azi
+
+    # We work with azi in [0, 2pi]
+    if azi_lower < 0:
+        azi_lower = azi_lower + 2*np.pi
+
+
+    # Handle inc_upper 0 or pi
+    # In these cases, the magnetic toolface is used intially
+    # In practice, the result are curves in a specific azimuthal direction,
+    # that is only build, no turn.
+    if inc_upper == 0 or inc_upper == np.pi:
+        azi_lower = toolface
+    
+    dnorth, deast, dtvd, dls = min_curve_segment(inc_upper, azi_upper, inc_lower, azi_lower, md_inc)
+
+    # Handle cases where we switch form magnetic to gravity toolface and so on
+    # 
+    # To to this, we need to return the toolface at the end of the segment
+    # For magnetic->gravity tf at end of segment will always be 
+    #   0 if inc_upper is 0 (only build angle)
+    #   pi if inc_upper is pi (only drop angle)
+    #
+    # To get to a situation were we switch form gravity to magentic toolface using 
+    # the dogleg toolface method, we've had to drill with no turn. Hence, in this case,
+    # the toolface at the end is actually the opposite direction of the azimuth at the start
+    tf_lower = toolface
+
+    # Magnetic to gravity toolface
+    if inc_upper == 0:
+        tf_lower = 0
+    elif inc_upper == np.pi:
+        tf_lower = np.pi
+
+    # Gravity to magnetic toolface
+    if inc_lower == 0 or inc_lower == np.pi:
+        tf_lower = azi_upper + 2*np.pi
+        if tf_lower > 2*np.pi:
+            tf_lower = tf_lower - 2*np.pi
+    
+    return np.array((dnorth, deast, dtvd, inc_lower, azi_lower, tf_lower))
+        
+
+if __name__ == '__main__':
+    
+    tf = np.linspace(0,2*np.pi, 100)
+    inc = np.copy(tf)
+    azi = np.copy(tf)
+    md = np.linspace(1,5000, 1000)
+    n = np.copy(md)
+    e = np.copy(md)
+    t = np.copy(md)
+    inc = np.copy(md)
+    azi = np.copy(md)
+    dls = 0.002
+    inc_upper = np.pi/2 #random()*np.pi
+    azi_upper = random()*2*np.pi
+    toolface = random()*2*np.pi
+
+    print("inc: ", inc_upper)
+    print("azi: ", azi_upper)
+    print("tf: ", toolface)
+
+    for i in range(0, len(md)):
+        n[i], e[i], t[i], inc[i], azi[i], _ = dogleg_toolface(inc_upper, azi_upper, toolface, dls, md[i])
 
     # plt.plot(tf, azi)
+    plt.figure()
+    plt.subplot(121)
+    plt.plot(md, n, 'r')
+    plt.plot(md, e, 'g')
+    plt.plot(md, t, 'b')
+    plt.subplot(122)
+    plt.plot(md, inc, 'k')
+    plt.plot(md, azi, 'y')
+    plt.show()
+
+    # theta = np.linspace(0, 2*np.pi, 1000)
+    # val_tan = np.copy(theta)
+    # val_sin = np.copy(theta)
+
+    # for i in range(0, len(theta)):
+    #     val_tan[i] = tan_delta(theta[i], toolface, inc_upper)
+    #     val_sin[i] = sin_delta(theta[i], toolface, inc_upper)
+    
+    # val_cos = val_sin / val_tan
+
+    # val_f = np.arctan2(val_sin, val_cos)
+
+    # for i in range(0, len(val_f)):
+    #     if val_f[i] < 0:
+    #         val_f[i] = val_f[i] + 2*np.pi
+
+    # val_tan = np.arctan(val_tan)
+
+    # plt.figure()
+    # plt.plot(theta, val_tan, 'r')
+    # plt.plot(theta, val_f, 'b')
     # plt.show()
+    data = dogleg_toolface(np.pi/2, np.pi, 0, 0.002, 300)
+    print(data)
