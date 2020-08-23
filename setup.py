b9e28d93510777f08bca607202629ec822f3677d
Jon Gustav Vabø <jgvabo@gmail.com>
2020-08-23 18:52:05 +0200

    Add barebone setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name="sti", packages=find_packages())
+setup(
+        name="sti",
+        packages=find_packages(),
+        install_requires=[
+            'sklearn',
+            'numpy',
+            'scipy',
+        ],
+        extras_require={
+            'train' : ['pandas'],
+        },
+        test_require=['pytest'],
+)
