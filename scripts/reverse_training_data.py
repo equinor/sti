b9cfa40ab8699aea666050dfb1705c428717fc4f
Jon Gustav Vabø <jgvabo@gmail.com>
2020-08-24 21:29:07 +0200

    Fix bug, not reversing directions. Add md check.

--- scripts/reverse_training_data.py
+++ scripts/reverse_training_data.py
@@ -2,6 +2,7 @@ import csv
 import numpy as np
 import pandas as pd
 from sti.sti_core import project_sti, standardize_pos, standardize_problem
+from sti.utils import cart_bit_from_state, net_to_spherical
 
 
 def reverse_training_data():
@@ -30,20 +31,39 @@ def reverse_training_data():
         writer.writerow(headers)
 
     for i in range(0, n_rows):
-        org_start =  df.iloc[i, 0:5]
-        org_target = df.iloc[i, 5:10]
+        org_start =  df.iloc[i, 0:5].values
+        org_target = df.iloc[i, 5:10].values
         dls = df.iloc[i,10]
-        org_sti = df.iloc[i, 11:17]
+        org_sti = df.iloc[i, 11:17].values
 
-        org_int_pos0 = df.iloc[i, 11:14]
-        org_int_pos1 = df.iloc[i, 14:17]
+        org_int_pos0 = df.iloc[i, 11:14].values
+        org_int_pos1 = df.iloc[i, 14:17].values
 
-        # Reverse start and target
-        start_state, target_state = standardize_problem(org_target, org_start)
+        # Store org md
+        _a, _b, org_md = project_sti(org_start, org_target, org_sti)
+
+        # TODO This is messy. Also should use list expansion *
+        # Reverse directions vectors
+        org_target_bit = cart_bit_from_state(org_target)
+        rev_target_bit = -1 * org_target_bit
+        rev_target_dir  = net_to_spherical(rev_target_bit[0], rev_target_bit[1], rev_target_bit[2])
+
+        rev_target = org_target[0:3]
+        rev_target = np.append(rev_target, rev_target_dir).flatten()
+
+        org_start_bit = cart_bit_from_state(org_start)
+        rev_start_bit = -1 * org_start_bit
+        rev_start_dir  = net_to_spherical(rev_start_bit[0], rev_start_bit[1], rev_start_bit[2])
+
+        rev_start = org_start[0:3]
+        rev_start = np.append(rev_start, rev_start_dir).flatten()
+
+        # Flip start and target
+        start_state, target_state = standardize_problem(rev_target, rev_start)
 
         # Transform and reverse intermediate points
-        int_pos0_stand = standardize_pos(org_target, org_start, org_int_pos1)
-        int_pos1_stand = standardize_pos(org_target, org_start, org_int_pos0)
+        int_pos0_stand = standardize_pos(rev_target, rev_start, org_int_pos1)
+        int_pos1_stand = standardize_pos(rev_target, rev_start, org_int_pos0)
 
         # Create sti
         sti = np.append(int_pos0_stand, int_pos1_stand).flatten()
@@ -54,7 +74,7 @@ def reverse_training_data():
         diff = target_state - projected_state
         sq_err = np.dot(diff, diff)
 
-        acceptable = (sq_err < 1e-3)
+        acceptable = (sq_err < 1e-3) and (abs(org_md - md) < 1.0 )
 
         if acceptable:
             print("Row : ",i, " Error below threshold. Storing data point.")
